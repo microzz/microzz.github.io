1eee784852c561c39e6d7a4f22ba7cd973668ca2
microzz <903036377@qq.com>
2019-07-15 22:13:01 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env sh
+cd /root/hexo-blog
 git pull
-hexo g -d
+hexo g
+cp -dpruf ./public/* /root/microzz.github.io
+time=$(date +%F)
+cd /root/microzz.github.io
+git add . && git commit -m 'update at $time' && git push
