43eeb8e963f3397b70edfef60de89fe220405757
microzhao <microzhao@tencent.com>
2019-07-16 00:32:35 +0800

    modify deploy.hs

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env sh
 cd /root/hexo-blog
 git pull
-hexo g
-cp -dpruf ./public/* /root/microzz.github.io
-time=$(date +%F)
-cd /root/microzz.github.io
-git add . && git commit -m 'update at $time' && git push
+hexo g -d
