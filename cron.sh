5b34bb0b36f70e4c20027bd92165852771c95774
microzone <microzone@MyMac.lan>
2022-03-25 01:43:43 +0800

    定时发布脚本

---
+++ cron.sh
@@ -0,0 +1,9 @@
+# * 12 * * * ~/codes/scripts/cron.sh
+# 0 20 * * 1,3,4 ~/codes/scripts/cron.sh
+# 0 21 5,15 * * ~/codes/scripts/cron.sh
+cd ~/codes/test
+t=`date '+%Y-%m-%d %H:%M:%S'`
+echo -e "$t \n" >> 'README.md'
+git add .
+git commit -m "$t"
+git push -f
