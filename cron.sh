24bbc1fbadbd5432b9db3fc0873690eba254fd4b
microzz <903036377@qq.com>
2022-03-25 01:47:49 +0800

    定时发布脚本

--- cron.sh
+++ cron.sh
@@ -2,7 +2,6 @@
 # * 12 * * * ~/codes/scripts/cron.sh
 # 0 20 * * 1,3,4 ~/codes/scripts/cron.sh
 # 0 21 5,15 * * ~/codes/scripts/cron.sh
-
 cd ~/codes/test
 t=`date '+%Y-%m-%d %H:%M:%S'`
 echo -e "$t \n" >> 'README.md'
